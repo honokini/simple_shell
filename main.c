aff2a0f7c59a52ba84d8b32cc0d83ece2cabc4b7
KINI Biè Honoré <104803215+honokini@users.noreply.github.com>
2023-03-07 00:31:45 +0100

    Updates files

--- main.c
+++ main.c
@@ -1,105 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include "main.h"
 
-void parseCommand(char *str, command *cmd);
-void executeCMD(command *cmd, char *const env[]);
+#include "shell.h"
 
 /**
- * main - Launch the shell programm.
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * @ac: Argument count, the number of arguments
- * passed to the program.
- *
- * @av: Argument list, a NULL terminated vector
- * of arguments values.
- *
- * @env: Environment List.
- *
- * Return: Int the exit status.
+ * Return: 0 on success, 1 on error
  */
-int main(int ac, char **av, char **env)
+int main(int ac, char **av)
 {
-	pid_t childPid;
-	command cmd;
-	int status = EXIT_SUCCESS;
-	size_t len;
-	ssize_t nread;
-	char *line = NULL;
-	char inputStr[50];
-	int i = 0;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	cmd.ppathname = av[0];
-	while ((ac > 1 && i == 0) || (ac == 1 && status == 0))
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
 	{
-		i++;
-		switch (childPid = fork())
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			case -1:
-				perror("Error");
-				status = 1;
-				break;
-			case 0:
-				if (ac > 1)
-				{
-					_strcpy(inputStr, av[1]);
-					parseCommand(inputStr, &cmd);
-
-					executeCMD(&cmd, env);
-				}
-				else
-				{
-					printf("#cisfun$ ");
-					while ((nread = getline(&line, &len, stdin)) != -1)
-					{
-						_strcpy(inputStr, line);
-						parseCommand(inputStr, &cmd);
-
-						executeCMD(&cmd, env);
-						printf("#cisfun$ ");
-					}
-
-					if (nread == -1)
-						_exit(EXIT_FAILURE);
-				}
-				_exit(EXIT_SUCCESS);
-				break;
-			default:
-				wait(&status);
-				break;
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
+		info->readfd = fd;
 	}
-
-	free(line);
-	exit(status);
-}
-
-/**
- * parseCommand - Parse a string into a command
- * type.
- *
- * @str: The string to parse.
- * @cmd: A pointer to a command struct.
- */
-void parseCommand(char *str, command *cmd)
-{
-	/* Currently only one command by string */
-	cmd->pathname = str;
-	cmd->args = malloc(2 * sizeof(cmd->args));
-	cmd->args[0] = str;
-	cmd->args[1] = NULL;
-}
-
-/**
- * executeCMD - Executes a shell command.
- * @cmd: A command pointer.
- * @env: Environment list.
- */
-void executeCMD(command *cmd, char *const env[])
-{
-	if (execve(cmd->pathname, cmd->args, env) == -1)
-		perror(cmd->ppathname);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
